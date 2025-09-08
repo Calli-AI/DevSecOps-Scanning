2c6301e543a596819a5284c9d14720271b4a4a38
Calli Thorley <calli.thorley@gmail.com>
2025-09-08 18:18:16 +0930

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "ap-southeast-2" # Sydney region
 }
+resource "aws_s3_bucket" "callisbucket" {
+ bucket = "callisbucket-12345"
+}
+terraform {
+    backend "s3" {
+        bucket = "callisbucket-12345"
+        key    = "global/s3/terraform.tfstate"
+        region = "ap-southeast-2"
+    }
+}
